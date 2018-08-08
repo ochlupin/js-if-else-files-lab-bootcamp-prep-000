6717af89e6d8dfc56cf6c1d51db6511ae6b24207
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-08 13:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var name = "Joe"
-var height = 70
+var height = "74"
 
-if (name === "Susan") {
-  console.log("The name is correct")
-} else {
-  console.log("Expected name to be Susan")
-};
 // Don't worry about this
 module.exports = {
   name,
